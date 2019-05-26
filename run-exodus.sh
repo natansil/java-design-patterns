8e1bb02f65f3c162381961b97f3af61993b8cd83
natans <natans@wix.com>
2019-05-26 23:01:38 +0300

    run-exodus

---
+++ run-exodus.sh
@@ -0,0 +1,7 @@
+java \
+-Drepo.root=/Users/natans/hackathon/java-design-patterns \
+-Drepo.url=git@github.com:iluwatar/java-design-patterns.git \
+-Dlocal.maven.repository.path=/Users/natans/.m2/repository \
+-Dskip.classpath=false \
+-Dskip.transformation=false \
+-jar ~/exodus.jar
